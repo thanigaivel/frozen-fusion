99ccc4a96a233bb9c2e6a783a5b35fa24936419f
sthanigaivel <sthanigaivel@gmail.com>
2026-08-11 20:57:43 +0530

    Increase mobile logo size

--- frozen-fusion/components/Navbar.tsx
+++ frozen-fusion/components/Navbar.tsx
@@ -10,7 +10,7 @@ export function Navbar() {
           alt="Frozen Fusion"
           width={300}
           height={110}
-          className="h-16 md:h-20 w-auto object-contain"
+          className="h-[72px] md:h-20 w-auto object-contain"
           priority
           fetchPriority="high"
         />
